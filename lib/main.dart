ce781998b48a4bf5518cff192c56596ede66b87a
bhavikcodezee <bhavik.codezee@gmail.com>
2023-05-18 12:32:17 +0530

    color

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Social & Payment  implement',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: HomeScreen(),
     );
